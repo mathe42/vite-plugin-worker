2b3a238759dabe0d5028d6243b0adef26143abab
Quincy <67159014+qu1ncyk@users.noreply.github.com>
2022-04-24 18:03:32 +0000

    fix paint.ts

--- src/worklets/paint.ts
+++ src/worklets/paint.ts
@@ -29,11 +29,11 @@ export function paint(inline_worklet_paint: boolean): Plugin {
 
       return PAINT_WORKLET_SCHEMA + realID.id;
     },
-    load(id) {
+    async load(id) {
       if (id === PAINT_WORKLET_HELPER) return PAINT_WORKLET_HELPER_CODE;
       if (!id.startsWith(PAINT_WORKLET_SCHEMA)) return;
 
-      const url = bundle(
+      const url = await bundle(
         this,
         id.slice(PAINT_WORKLET_SCHEMA.length),
         "worklet@paint",
